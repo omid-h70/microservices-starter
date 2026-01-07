7b79e68ad11aa7c766fa906ed62cf70bc6f68de2
omid-h70 <stalkeromid2142@gmail.com>
2026-01-07 10:13:09 -0500

    WIP from sadad

--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -7,15 +7,46 @@ import (
 )
 
 type RabbitMQ struct {
-	conn *amqp.Connection
+	conn    *amqp.Connection
+	Channel *amqp.Channel
 }
 
 func NewRabbitMQ(uri string) (*RabbitMQ, error) {
 	conn, err := amqp.Dial(uri)
 	if err != nil {
-		return nil, fmt.Errorf("faild to connect to rabbit mq %w", err)
+		return nil, fmt.Errorf("faild to connect to rabbitmq %w", err)
 	}
-	return &RabbitMQ{
-		conn: conn,
-	}, nil
+
+	channel, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("faild to create rabbitmq channel %w", err)
+	}
+
+	rmq := &RabbitMQ{
+		conn:    conn,
+		Channel: channel,
+	}
+
+	if err := rmq.setupExchangesAndQueues(); err != nil {
+
+		rmq.Close()
+		return nil, fmt.Errorf("faild to setup exchanges or queues %w", err)
+	}
+
+	return rmq, nil
+}
+
+func (amap *RabbitMQ) setupExchangesAndQueues() error {
+	return nil
+}
+
+func (amqp *RabbitMQ) Close() {
+	if amqp.Channel != nil {
+		amqp.Channel.Close()
+	}
+
+	if amqp.conn != nil {
+		amqp.conn.Close()
+	}
 }
